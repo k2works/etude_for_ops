de23fbcdee331116944cecd7c16158cf92b12669
k2works <kakimomokuri@gmail.com>
2017-08-31 14:33:57 +0900

    インターフェース設計

--- spec/ops_spec.rb
+++ spec/ops_spec.rb
@@ -1,7 +1,31 @@
 require 'rspec'
 
 describe 'EtudeForOps::Ops' do
-  it 'should do something' do
-    true.should == false
+  context 'onpremis' do
+    describe '#create_onpremis_development_env' do
+      it 'create development environment'
+    end
+
+    describe '#create_onpremis_staging_env' do
+      it 'create staging environment'
+    end
+
+    describe '#create_onpremis_production_env' do
+      it 'create production environment'
+    end
+  end
+
+  context 'cloud' do
+    describe '#create_aws_development_env' do
+      it 'create development environment'
+    end
+
+    describe '#create_aws_staging_env' do
+      it 'create staging environment'
+    end
+
+    describe '#create_aws_production_env' do
+      it 'create production environment'
+    end
   end
 end
