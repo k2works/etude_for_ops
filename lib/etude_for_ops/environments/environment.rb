4fb8e73a229aef11ee61474a11b2da26277e4aea
k2works <kakimomokuri@gmail.com>
2017-09-04 11:24:00 +0900

    リファクタリング
    ・ディレクトリ作成モジュール分離

--- lib/etude_for_ops/environments/environment.rb
+++ lib/etude_for_ops/environments/environment.rb
@@ -1,5 +1,9 @@
+require_relative '../utiles/dir_utile'
+
 module EtudeForOps
   class Environment
+    include DirUtile
+
     attr_accessor :platform,
                   :source,
                   :config,
@@ -35,42 +39,6 @@ module EtudeForOps
 
     def fullname
       self.class.to_s
-    end
-
-    def make_root_dir
-      FileUtils.mkdir_p(@root_dir, mode:0755)
-    end
-
-    def make_env_dir
-      FileUtils.mkdir_p(@env_dir, mode:0755)
-    end
-
-    def make_env_config_dir
-      FileUtils.mkdir_p(@config_dir, mode:0755)
-    end
-
-    def make_env_config_env_dir
-      FileUtils.mkdir_p(@config_env_dir, mode:0755)
-    end
-
-    def make_env_config_secure_dir
-      FileUtils.mkdir_p(@config_secure_dir, mode:0755)
-    end
-
-    def make_env_src_dir
-      FileUtils.mkdir_p(@src_dir, mode:0755)
-    end
-
-    def make_env_src_build_dir
-      FileUtils.mkdir_p(@src_build_dir, mode:0755)
-    end
-
-    def make_env_src_ship_dir
-      FileUtils.mkdir_p(@src_ship_dir, mode:0755)
-    end
-
-    def make_env_src_run_dir
-      FileUtils.mkdir_p(@src_run_dir, mode:0755)
     end
 
     def ops_yml_file_path
