6d23b1feba67007ebfac6fc00128ae99677e6d90
k2works <kakimomokuri@gmail.com>
2017-10-11 11:22:35 +0900

    リファクタリング #EFOPS-5

--- lib/etude_for_ops/ops.rb
+++ lib/etude_for_ops/ops.rb
@@ -8,98 +8,123 @@ module EtudeForOps
     end
 
     def create_onpremis_ruby_development_env(root_dir)
-      env = Development.new(root_dir)
-      @builder.strategy = Onpremis.new(env)
+      begin
+        env = Development.new(root_dir)
+        @builder.strategy = Onpremis.new(env)
 
-      ruby = Ruby.new(env)
-      ruby.components << EtudeForOps::Shell.new(ruby)
-      ruby.components << EtudeForOps::Chef.new(ruby)
-      ruby.components << EtudeForOps::Capistrano.new(ruby)
-      ruby.components << EtudeForOps::Rake.new(ruby)
-      ruby.components << EtudeForOps::Configure.new(ruby)
-      ruby.components << EtudeForOps::Vagrant.new(ruby)
-      @builder.platforms <<  ruby
+        ruby = Ruby.new(env)
+        ruby.components << EtudeForOps::Shell.new(ruby)
+        ruby.components << EtudeForOps::Chef.new(ruby)
+        ruby.components << EtudeForOps::Capistrano.new(ruby)
+        ruby.components << EtudeForOps::Rake.new(ruby)
+        ruby.components << EtudeForOps::Configure.new(ruby)
+        ruby.components << EtudeForOps::Vagrant.new(ruby)
+        @builder.platforms <<  ruby
 
-      @builder.environment = env
-      @builder.apply_strategy
-      return @builder.environment, @builder.platforms, @builder.strategy
+        @builder.environment = env
+        @builder.apply_strategy
+        return @builder.environment, @builder.platforms, @builder.strategy
+      rescue Exception => e
+        puts "Error occurred (#{e.class})"
+        throw e
+      end
     end
 
     def create_onpremis_ruby_staging_env(root_dir)
-      env = Staging.new(root_dir)
-      @builder.strategy = Onpremis.new(env)
+      begin
+        env = Staging.new(root_dir)
+        @builder.strategy = Onpremis.new(env)
 
-      ruby = Ruby.new(env)
-      ruby.components << EtudeForOps::Shell.new(ruby)
-      ruby.components << EtudeForOps::Chef.new(ruby)
-      ruby.components << EtudeForOps::Capistrano.new(ruby)
-      ruby.components << EtudeForOps::Rake.new(ruby)
-      ruby.components << EtudeForOps::Configure.new(ruby)
-      ruby.components << EtudeForOps::Vagrant.new(ruby)
-      @builder.platforms <<  ruby
+        ruby = Ruby.new(env)
+        ruby.components << EtudeForOps::Shell.new(ruby)
+        ruby.components << EtudeForOps::Chef.new(ruby)
+        ruby.components << EtudeForOps::Capistrano.new(ruby)
+        ruby.components << EtudeForOps::Rake.new(ruby)
+        ruby.components << EtudeForOps::Configure.new(ruby)
+        ruby.components << EtudeForOps::Vagrant.new(ruby)
+        @builder.platforms <<  ruby
 
-      @builder.environment = env
-      @builder.apply_strategy
-      return @builder.environment, @builder.platforms, @builder.strategy
+        @builder.environment = env
+        @builder.apply_strategy
+        return @builder.environment, @builder.platforms, @builder.strategy
+      rescue Exception => e
+        puts "Error occurred (#{e.class})"
+        throw e
+      end
     end
 
     def create_onpremis_ruby_production_env(root_dir)
-      env = Production.new(root_dir)
-      @builder.strategy = Onpremis.new(env)
+      begin
+        env = Production.new(root_dir)
+        @builder.strategy = Onpremis.new(env)
 
-      ruby = Ruby.new(env)
-      ruby.components << EtudeForOps::Shell.new(ruby)
-      ruby.components << EtudeForOps::Chef.new(ruby)
-      ruby.components << EtudeForOps::Capistrano.new(ruby)
-      ruby.components << EtudeForOps::Rake.new(ruby)
-      ruby.components << EtudeForOps::Configure.new(ruby)
-      ruby.components << EtudeForOps::Vagrant.new(ruby)
-      @builder.platforms <<  ruby
+        ruby = Ruby.new(env)
+        ruby.components << EtudeForOps::Shell.new(ruby)
+        ruby.components << EtudeForOps::Chef.new(ruby)
+        ruby.components << EtudeForOps::Capistrano.new(ruby)
+        ruby.components << EtudeForOps::Rake.new(ruby)
+        ruby.components << EtudeForOps::Configure.new(ruby)
+        ruby.components << EtudeForOps::Vagrant.new(ruby)
+        @builder.platforms <<  ruby
 
-      @builder.environment = env
-      @builder.apply_strategy
-      return @builder.environment, @builder.platforms, @builder.strategy
+        @builder.environment = env
+        @builder.apply_strategy
+        return @builder.environment, @builder.platforms, @builder.strategy
+      rescue Exception => e
+        puts "Error occurred (#{e.class})"
+        throw e
+      end
     end
 
     def create_onpremis_ruby_share_env(root_dir)
-      env = Share.new(root_dir)
-      @builder.strategy = Onpremis.new(env)
+      begin
+        env = Share.new(root_dir)
+        @builder.strategy = Onpremis.new(env)
 
-      ruby = Ruby.new(env)
-      ruby.components << EtudeForOps::Shell.new(ruby)
-      ruby.components << EtudeForOps::Chef.new(ruby)
-      ruby.components << EtudeForOps::Capistrano.new(ruby)
-      ruby.components << EtudeForOps::Rake.new(ruby)
-      ruby.components << EtudeForOps::Configure.new(ruby)
-      ruby.components << EtudeForOps::Vagrant.new(ruby)
-      @builder.platforms <<  ruby
+        ruby = Ruby.new(env)
+        ruby.components << EtudeForOps::Shell.new(ruby)
+        ruby.components << EtudeForOps::Chef.new(ruby)
+        ruby.components << EtudeForOps::Capistrano.new(ruby)
+        ruby.components << EtudeForOps::Rake.new(ruby)
+        ruby.components << EtudeForOps::Configure.new(ruby)
+        ruby.components << EtudeForOps::Vagrant.new(ruby)
+        @builder.platforms <<  ruby
 
-      @builder.environment = env
-      @builder.apply_strategy
-      return @builder.environment, @builder.platforms, @builder.strategy
+        @builder.environment = env
+        @builder.apply_strategy
+        return @builder.environment, @builder.platforms, @builder.strategy
+      rescue Exception => e
+        puts "Error occurred (#{e.class})"
+        throw e
+      end
     end
 
     def create_aws_ruby_development_env(root_dir)
-      env = Development.new(root_dir)
-      aws = EtudeForOps::AWS.new(env)
+      begin
+        env = Development.new(root_dir)
+        aws = EtudeForOps::AWS.new(env)
 
-      @builder.strategy = Cloud.new(aws,env)
-      aws.components << EtudeForOps::EC2.new(aws)
-      aws.components << EtudeForOps::VPC.new(aws)
-      @builder.platforms << aws
+        @builder.strategy = Cloud.new(aws,env)
+        aws.components << EtudeForOps::EC2.new(aws)
+        aws.components << EtudeForOps::VPC.new(aws)
+        @builder.platforms << aws
 
-      ruby = Ruby.new(env)
-      ruby.components << EtudeForOps::Shell.new(ruby)
-      ruby.components << EtudeForOps::Chef.new(ruby)
-      ruby.components << EtudeForOps::Capistrano.new(ruby)
-      ruby.components << EtudeForOps::Rake.new(ruby)
-      ruby.components << EtudeForOps::Configure.new(ruby)
-      ruby.components << EtudeForOps::Vagrant.new(ruby)
-      @builder.platforms <<  ruby
+        ruby = Ruby.new(env)
+        ruby.components << EtudeForOps::Shell.new(ruby)
+        ruby.components << EtudeForOps::Chef.new(ruby)
+        ruby.components << EtudeForOps::Capistrano.new(ruby)
+        ruby.components << EtudeForOps::Rake.new(ruby)
+        ruby.components << EtudeForOps::Configure.new(ruby)
+        ruby.components << EtudeForOps::Vagrant.new(ruby)
+        @builder.platforms <<  ruby
 
-      @builder.environment = env
-      @builder.apply_strategy
-      return @builder.environment, @builder.platforms, @builder.strategy
+        @builder.environment = env
+        @builder.apply_strategy
+        return @builder.environment, @builder.platforms, @builder.strategy
+      rescue Exception => e
+        puts "Error occurred (#{e.class})"
+        throw e
+      end
     end
 
     def create_aws_ruby_staging_env(root_dir)
