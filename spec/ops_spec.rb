36c6a939e3f2fdd17041b35bd24a09ed06c3a226
k2works <kakimomokuri@gmail.com>
2017-10-27 14:35:06 +0900

    クラウド開発環境テスト追加 #EFOPS-5

--- spec/ops_spec.rb
+++ spec/ops_spec.rb
@@ -318,14 +318,202 @@ describe 'EtudeForOps::Ops' do
             expect(env_dir_file).to exist
             expect(vagrant_file).to exist
             expect(set_dev_env_file).to exist
+          end
 
+          it 'create config files' do
+            config_dir = Pathname.new(dir[:env_conf_dir] + 'env')
+            expect(config_dir).to exist
+
+            config_files = %w(
+           set-dev-env.sh
+           set-share-env.sh
+           )
+            config_files.each do |file|
+              expect(Pathname.new(config_dir + file)).to exist
+            end
+          end
+
+          it 'create build chef files' do
             chef_dir = Pathname.new(dir[:env_src_build_dir] + 'chef')
             expect(chef_dir).to exist
-            chef_file = Pathname.new(chef_dir + 'provision.json')
-            expect(chef_file).to exist
 
+            chef_files = %w[
+             attributes_default.rb
+             metadata.rb
+             provision.json
+             ec2_provision.json
+             recipes_default.rb
+             recipes_ec2.rb
+             recipes_setup.rb
+             recipes_configure.rb
+             recipes_configure_service.rb
+             recipes_setup_ruby.rb
+             recipes_setup_mysql.rb
+             recipes_setup_postgres.rb
+             recipes_setup_redis.rb
+             recipes_setup_mongodb.rb
+             recipes_setup_node.rb
+             recipes_setup_nginx.rb
+             recipes_setup_service.rb
+             recipes_setup_td-agent.rb
+             recipes_setup_elasticsearch.rb
+             recipes_setup_docker.rb
+             recipes_setup_jenkins.rb
+             recipes_setup_aws.rb
+             recipes_setup_vagrant.rb
+             Vagrantfile
+             .kitchen.yml
+             Gemfile
+             reprovision.sh
+            ]
+
+            chef_files.each do |file|
+              expect(Pathname.new(chef_dir + file)).to exist
+            end
+
+            chef_erb_template_files = %w[
+             templates_default_banner.erb
+             templates_default_bash_profile.erb
+             templates_default_bash_profile_aws.erb
+             templates_default_grants.sql.erb
+             templates_default_my_extra_settings.erb
+             templates_default_nginx.conf.erb
+             templates_default_nginx.default.conf.erb
+             templates_default_nginx.proxy.conf.erb
+             templates_default_nginx.kibana.conf.erb
+             templates_default_nginx.jenkins.conf.erb
+             templates_default_puma.erb
+             templates_default_puma.service.erb
+             templates_default_active_job.erb
+             templates_default_active_job.service.erb
+             templates_default_td-agent-ui.erb
+             templates_default_td-agent-ui.service.erb
+             templates_default_kibana.yml.erb
+             templates_default_jenkins.erb
+            ]
+
+            chef_erb_template_files.each do |file|
+              expect(Pathname.new(chef_dir + file)).to exist
+            end
+          end
+
+          it 'create build sh files' do
             sh_dir = Pathname.new(dir[:env_src_build_dir] + 'sh')
             expect(sh_dir).to exist
+
+            sh_files = %w[
+          build.sh
+          ]
+
+            sh_files.each do |file|
+              expect(Pathname.new(sh_dir + file)).to exist
+            end
+          end
+
+          it 'create ship capistrano files' do
+            cap_dir = Pathname.new(dir[:env_src_ship_dir] + 'capistrano')
+            cap_puma_dir = Pathname.new(cap_dir + 'puma')
+            cap_tasks_dir = Pathname.new(cap_dir + 'tasks')
+            cap_td_agent_dir = Pathname.new(cap_dir + 'td-agent')
+            expect(cap_dir).to exist
+            expect(cap_puma_dir).to exist
+            expect(cap_tasks_dir).to exist
+            expect(cap_td_agent_dir).to exist
+
+            cap_puma_files = %w[
+             Capfile
+             deploy.rb
+             development.rb
+             maintenance.html.erb
+           ]
+            cap_tasks_files = %w[
+             active_job.rake
+             db.rake
+             puma.rake
+             td-agent.rake
+           ]
+            cap_td_agent_files = %w[
+             fluent-logger.yml
+             td-agent.conf
+           ]
+
+            cap_puma_files.each do |file|
+              expect(Pathname.new(cap_puma_dir + file)).to exist
+            end
+            cap_tasks_files.each do |file|
+              expect(Pathname.new(cap_tasks_dir + file)).to exist
+            end
+            cap_td_agent_files.each do |file|
+              expect(Pathname.new(cap_td_agent_dir + file)).to exist
+            end
+          end
+
+          it 'create ship sh files' do
+            sh_dir = Pathname.new(dir[:env_src_ship_dir] + 'sh')
+            expect(sh_dir).to exist
+
+            sh_files = %w[
+          ship.sh
+          ]
+
+            sh_files.each do |file|
+              expect(Pathname.new(sh_dir + file)).to exist
+            end
+          end
+
+          it 'create ship vagrant files' do
+            vagrant_dir = Pathname.new(dir[:env_src_ship_dir] + 'vagrant')
+            expect(vagrant_dir).to exist
+
+            vagrant_files = %w[
+             create_box.sh
+             metadata.json
+            ]
+
+            vagrant_files.each do |file|
+              expect(Pathname.new(vagrant_dir + file)).to exist
+            end
+          end
+
+          it 'create run config files' do
+            config_dir = Pathname.new(dir[:env_src_run_dir] + 'config')
+            expect(config_dir).to exist
+
+            config_files = %w[
+             schedule.rb
+             sidekiq.rb
+             sidekiq.yml
+            ]
+
+            config_files.each do |file|
+              expect(Pathname.new(config_dir + file)).to exist
+            end
+          end
+
+          it 'create run rake files' do
+            rake_dir = Pathname.new(dir[:env_src_run_dir] + 'rake')
+            expect(rake_dir).to exist
+
+            rake_files = %w[
+           db.rake
+          ]
+
+            rake_files.each do |file|
+              expect(Pathname.new(rake_dir + file)).to exist
+            end
+          end
+
+          it 'create run sh files' do
+            sh_dir = Pathname.new(dir[:env_src_run_dir] + 'sh')
+            expect(sh_dir).to exist
+
+            sh_files = %w[
+             run.sh
+            ]
+
+            sh_files.each do |file|
+              expect(Pathname.new(sh_dir + file)).to exist
+            end
           end
         end
 
